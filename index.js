610b18d7c5b97f9e7b88ef80e603e91d24c877ce
Ryan Smith <ryanshaunsmith@gmail.com>
2025-08-28 12:27:55 +0100

    Add files via upload
    Code completed on Scrimba Platform

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+const generateBtn = document.getElementById("generate-btn")
+const outputOne = document.getElementById("output-one")
+const outputTwo = document.getElementById("output-two")
+
+function getRandomChar() {
+    return characters[Math.floor(Math.random() * characters.length)]
+}
+
+function generateRandomPassword() {
+    let password = "";
+    for (let i = 1; i <= 15; i++) {
+        password += getRandomChar();
+    }
+    return password;    
+}
+
+function populatePasswords() {
+    outputOne.textContent = generateRandomPassword();
+    outputTwo.textContent = generateRandomPassword();
+}
+
+generateBtn.addEventListener("click", populatePasswords)
